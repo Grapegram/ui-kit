789746c048bf337784bd68261da9f2463497f217
Kyrylo Ulianov <kirillulianow@gmail.com>
2025-09-30 21:32:36 +0200

    chore: added basic configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,7 +12,7 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/vue3-vite',
     options: {
-       docgen: 'vue-component-meta',
+      docgen: 'vue-component-meta',
     },
   },
 }
