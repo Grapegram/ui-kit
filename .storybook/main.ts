21f1db9184348063bf60220a9162a3c373bef5be
Kyrylo Ulianov <kirillulianow@gmail.com>
2025-11-20 16:18:21 +0100

    refactor: updated project configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,6 @@ import type { StorybookConfig } from '@storybook/vue3-vite'
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@chromatic-com/storybook',
     '@storybook/addon-docs',
     '@storybook/addon-onboarding',
     '@storybook/addon-a11y',
