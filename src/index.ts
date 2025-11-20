69c5331ac5786bcbbece7fd342f71e3a12bbb511
Kyrylo Ulianov <kirillulianow@gmail.com>
2025-11-20 19:46:38 +0100

    feat: let components to be exported from package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import './styles/main.css'
 
+export * from './components/avatar'
+export * from './components/badge'
 export * from './components/button'
+export * from './components/input'
+export * from './components/typography'
 export * from './lib'
