945c0fb935e267e4a7b86f84d7247747b47ba87f
Kyrylo Ulianov <kirillulianow@gmail.com>
2025-11-23 15:28:20 +0100

    feat: added font styles and update imports in main.css

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,10 @@ export default defineConfig({
           src: 'src/styles/theme.css',
           dest: 'styles',
         },
+        {
+          src: 'src/styles/fonts.css',
+          dest: 'styles',
+        },
       ],
     }),
   ],
